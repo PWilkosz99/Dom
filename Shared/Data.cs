84471a58981a0beb630dabc22080c84e740b5a1f
Szymon <szymon_0@hotmail.com>
2018-09-05 00:09:29 +0200

    1st

---
+++ Shared/Data.cs
@@ -0,0 +1,25 @@
+﻿#define ALT // alternatywne dane
+
+using System;
+
+#if (SRV)
+namespace Serwer
+#elif (WINDOWS_UWP)
+namespace Dom_Background
+#elif(ANDROID)
+namespace Dom_android
+#endif
+{
+    public static class Data
+    {
+#if (ALT)
+        private static string _serwerIp = "192.168.43.163";
+        private static int _serwerPort = 80;
+#else
+        public static string _serwerIp = "192.168.1.4";
+        public static int _serwerPort = 80;
+#endif
+        public static string SerwerIp { get => _serwerIp; }
+        public static int SerwerPort { get => _serwerPort;}
+    }
+}
