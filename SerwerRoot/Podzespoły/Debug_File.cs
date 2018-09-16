b0b68dc109cb570b5c4d4e2d30c46269adfefbde
Szymon <szymon_0@hotmail.com>
2018-09-16 02:24:53 +0200

    Dodanie Projektu Serwer z UI

---
+++ SerwerRoot/Podzespoły/Debug_File.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serwer.Podzespoły
+{
+    class Debug_File
+    {
+        StreamWriter sw;
+        public Debug_File()
+        {
+            sw = new StreamWriter(new FileStream(@"C:\Debug.txt", FileMode.OpenOrCreate));
+            WriteAsync("------------------------------------------------------------------------");
+        }
+        ~Debug_File()
+        {
+            sw.Dispose();
+        }
+        public async void WriteAsync(string text)
+        {
+            await sw.WriteLineAsync(DateTime.Now + ": " + text);
+        }
+    }
+}
